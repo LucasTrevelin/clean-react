7c1ccdd7bdec51e19f27c01c8ba3eba6d08f6be7
Lucas Henrique Trevelin <lhenriquetreveli@apple.com>
2024-03-05 12:18:26 -0300

    chore: adds vitest

---
+++ clean-react/test.setup.ts
@@ -0,0 +1,44 @@
+import './src/i18n'
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/vitest'
+
+import { cleanup } from '@testing-library/react'
+import { noop } from 'lodash'
+import { afterEach, vi } from 'vitest'
+
+vi.mock('react-router-dom', async (importOriginal) => ({
+  ...((await importOriginal()) as object),
+  unstable_useBlocker: () => ({
+    state: '',
+    reset: noop
+  })
+}))
+
+afterEach(() => {
+  cleanup()
+})
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn()
+  }))
+})
+
+const { getComputedStyle } = window
+window.getComputedStyle = (elt) => getComputedStyle(elt)
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn()
+}))
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock)
--- clean-react/vite.config.ts
+++ clean-react/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test.setup.ts',
+    reporters: ['default', 'html'],
+    coverage: {
+      all: true,
+      provider: 'istanbul', // or 'v8'
+      exclude: ['src/__mocks__/', 'html/**', 'public/**', '*.cjs'],
+      reporter: ['text', 'html', 'lcov']
+    },
+    chaiConfig: {
+      truncateThreshold: Infinity
+    }
+  }
 })
